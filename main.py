c405388fa69d523d94f2feec3280066838bb4247
Alpha-x-prog <76827289+Alpha-x-prog@users.noreply.github.com>
2021-11-20 02:51:28 +0300

    typing class part 4

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ key_board = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "
              "-", "=", "\"", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 first_side = ['Ё', 'Ё', 'Ц', 'У', 'К', 'Е', 'Ф', 'Ы', 'В', 'А', 'П', 'Я', 'Ч', 'С', 'М', 'И', '1', '2', '3', '4', '5']
 additional_characters = {ord('"'): ['Left_Shift', 2], ord(':'): ['Right_Shift', 6], ord(","): ['Right_Shift', '.']}
+lessons_letters = {1: ['ф', 'ы', 'в', 'а', 'о', 'л', 'д', 'ж'], 2: ['п', 'р']}
 
 
 # conn.close()
@@ -32,92 +33,22 @@ class SwitchBetweenButtons(QtWidgets.QMainWindow):
         self.user_test.show()
         self.close()
 
-    def lessons(self, lesson_number=1, part=1):
-        self.user_lessons = UserLessons()
+    def lessons(self, lesson_number, part):
+        self.user_lessons = UserLessons(lesson_number, part)
         self.user_lessons.show()
         self.close()
 
 
-class FirstWindow(SwitchBetweenButtons):
+class ButtonsClick(SwitchBetweenButtons):
     def __init__(self):
         super().__init__()
-        self.window = None
-        self.general_window = None
-        self.user_name = None
-        self.init_ui()
-
-    def init_ui(self):
-        super(FirstWindow, self).__init__()
-        self.window = uic.loadUi('qt_designer/first_window.ui', self)
-        self.window.setWindowTitle('Введите имя')
-        self.show()
-        self.window.btnfinished.clicked.connect(self.btn_clicked)
-
-    def btn_clicked(self):
-        result_name = self.window.user_name_label.text()
-        if result_name:
-            cursor.execute('INSERT INTO user_informational '
-                           'VAlUES (?)', (result_name,))
-            conn.commit()
-            self.general_window = GeneralWindow()
-            self.general_window.show()
-            self.close()
-        else:
-            self.window.warning.setText("нельзя войти с пустым именем")
-
-
-class GeneralWindow(SwitchBetweenButtons):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
-
-    def init_ui(self):
-        super(GeneralWindow, self).__init__()
-        self.window = uic.loadUi('qt_designer/general information.ui', self)
-        self.window.setWindowTitle('Общее')
-        self.show()
-        self.window.btn_testing.clicked.connect(self.testing)
-        self.window.btn_user_lessons.clicked.connect(self.lessons)
-
-
-class Test(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.window = None
-        self.count_pressed = 0
-        self.text = None
-        self.first_color_button = None
         self.dop_button_color = None
         self.first_button = None
-        self.shift_button = 0
-        self.symbols = None
-        self.red_button = None
-        self.red_button_color = None
-        self.result_typ = None
-        self.count_time = 0
-        self.mistakes = 0
-        self.timer = None
         self.first_press = 1
-        self.init_ui()
-
-    def init_ui(self):
-        super(Test, self).__init__()
-        self.window = uic.loadUi('qt_designer/test.ui', self)
-        self.window.setWindowTitle('Проверка скорости')
-
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.showTime)
-
-        count_str = cursor.execute('SELECT COUNT(1) '
-                                   'FROM texts').fetchone()[0]
-        id_text = random.randint(1, count_str)
-        self.text, self.symbols = cursor.execute('SELECT text, symbols '
-                                                 'FROM texts '
-                                                 'WHERE id = ?', (5,)).fetchone()  # id_text
-        self.window.for_text.setPlainText(self.text)
-        self.show()
-        self.change_color_button_green(ord(self.text[self.count_pressed]))
-        self.window.user_text.installEventFilter(self)
+        self.result_typ = None
+        self.first_color_button = None
+        self.shift_button = None
+        self.shift = None
 
     def eventFilter(self, obj, event):
         if obj is self.window.user_text and event.type() == QtCore.QEvent.KeyPress:
@@ -176,6 +107,80 @@ class Test(QtWidgets.QMainWindow):
         self.window.time.setText(str(self.count_time // 60) + "." + str(self.count_time % 60).zfill(2))
 
 
+class FirstWindow(SwitchBetweenButtons):
+    def __init__(self):
+        super().__init__()
+        self.window = None
+        self.general_window = None
+        self.user_name = None
+        self.init_ui()
+
+    def init_ui(self):
+        super(FirstWindow, self).__init__()
+        self.window = uic.loadUi('qt_designer/first_window.ui', self)
+        self.window.setWindowTitle('Введите имя')
+        self.show()
+        self.window.btnfinished.clicked.connect(self.btn_clicked)
+
+    def btn_clicked(self):
+        result_name = self.window.user_name_label.text()
+        if result_name:
+            cursor.execute('INSERT INTO user_informational '
+                           'VAlUES (?)', (result_name,))
+            conn.commit()
+            self.general_window = GeneralWindow()
+            self.general_window.show()
+            self.close()
+        else:
+            self.window.warning.setText("нельзя войти с пустым именем")
+
+
+class GeneralWindow(SwitchBetweenButtons):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        super(GeneralWindow, self).__init__()
+        self.window = uic.loadUi('qt_designer/general information.ui', self)
+        self.window.setWindowTitle('Общее')
+        self.show()
+        self.window.btn_testing.clicked.connect(self.testing)
+        self.window.btn_user_lessons.clicked.connect(lambda: self.lessons(1, 1))
+
+
+class Test(ButtonsClick):
+    def __init__(self):
+        super().__init__()
+        self.window = None
+        self.count_pressed = 0
+        self.text = None
+        self.symbols = None
+        self.count_time = 0
+        self.mistakes = 0
+        self.timer = None
+        self.init_ui()
+
+    def init_ui(self):
+        super(Test, self).__init__()
+        self.window = uic.loadUi('qt_designer/test.ui', self)
+        self.window.setWindowTitle('Проверка скорости')
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.showTime)
+
+        count_str = cursor.execute('SELECT COUNT(1) '
+                                   'FROM texts').fetchone()[0]
+        id_text = random.randint(1, count_str)
+        self.text, self.symbols = cursor.execute('SELECT text, symbols '
+                                                 'FROM texts '
+                                                 'WHERE id = ?', (5,)).fetchone()  # id_text
+        self.window.for_text.setPlainText(self.text)
+        self.show()
+        self.change_color_button_green(ord(self.text[self.count_pressed]))
+        self.window.user_text.installEventFilter(self)
+
+
 class ResultTyping(QtWidgets.QMainWindow):
     def __init__(self, time, mistakes, symbols):
         super().__init__()
@@ -192,7 +197,7 @@ class ResultTyping(QtWidgets.QMainWindow):
             self.time_typing % 60, 'секунд'),
 
         self.window.time.setText(f'{minutes} {second}')
-        self.window.speed.setText(f'{math.ceil(self.symbols // self.time_typing)} симв/мин')
+        self.window.speed.setText(f'{math.ceil(self.symbols // self.time_typing * 60)} симв/мин')
         self.window.mistakes.setText(f'{self.user_mistakes}')
         print(self.user_mistakes, self.symbols)
         self.window.purity.setText(f'{format((self.symbols - self.user_mistakes) / self.symbols * 100, ".2f")}%')
@@ -209,21 +214,39 @@ class ResultTyping(QtWidgets.QMainWindow):
 
 
 class UserLessons(SwitchBetweenButtons):
-    def __init__(self):
+    def __init__(self, lesson_number, part):
         super().__init__()
-        self.init_ui()
+        self.symbols = []
+        self.init_ui(lesson_number, part)
 
-    def init_ui(self):
+    def init_ui(self, lesson_number, part):
         super(UserLessons, self).__init__()
         self.window = uic.loadUi('qt_designer/user_lessons.ui', self)
+        for i in range(lesson_number):
+            for j in range(len(lessons_letters.get(i + 1))):
+                self.symbols.append(lessons_letters.get(i + 1)[j])
+        if part == 1:
+            print(random.choices((self.symbols), weights=[10, 20000, 10, 10, 10, 20, 10, 10]))
+        elif part == 2:
+            pass
+        else:
+            pass
+        #  self.window.for_text.setPlainText("".join(self.symbols[::]))
         for i in range(3):
             self.window.findChild(QComboBox, str('lesson_' + str((i + 1)))).activated[str].connect(self.number_lesson)
         #  self.window.findChild(QComboBox, str('lesson_' + str((i + 1)))).currentIndexChanged[str].connect(self.number_lesson)
 
     def number_lesson(self):
         changed_text_box = str(self.sender().currentText())
-        print(changed_text_box[changed_text_box.rfind(" ") + 1:])
-        self.lessons(1, 1)
+        print(changed_text_box[changed_text_box.rfind(" ") - 2], changed_text_box[changed_text_box.rfind(" ") + 1:])
+        part = changed_text_box[changed_text_box.rfind(" ") + 1:]
+        if part == 'Буквы':
+            part_lesson = 1
+        elif part == 'Клавишы':
+            part_lesson = 2
+        else:
+            part_lesson = 3
+        self.lessons(int(changed_text_box[changed_text_box.rfind(" ") - 2]), part_lesson)
 
 
 if __name__ == '__main__':
